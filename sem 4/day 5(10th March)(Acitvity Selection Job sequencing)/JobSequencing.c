91995cb31d48ccd2c5c92133262bad2f18483a2a
Akash Kinkar Pandey <104554525+akashkinkarpandey@users.noreply.github.com>
2022-06-15 16:37:12 +0530

    Job sequencing updated full

--- sem 4/day 5(10th March)(Acitvity Selection Job sequencing)/JobSequencing.c
+++ sem 4/day 5(10th March)(Acitvity Selection Job sequencing)/JobSequencing.c
@@ -1,23 +1,36 @@
-#include <stdio.h>
-#include <limits.h>
-void sort(int id[], int profit[], int deadline[], int n)
+// #include<stdio.h>
+// #include<limits.h>
+// Enter number of items
+// 5
+// Enter id,profit,deadline
+// 1 100 2
+// Enter id,profit,deadline
+// 2 19 1
+// Enter id,profit,deadline
+// 3 27 2
+// Enter id,profit,deadline
+// 4 25 1
+// Enter id,profit,deadline
+// 5 15 3
+// Jobs done are ->3,Profit is->142
+// Job ids order in which they were performed are given -> 1 3 5
+void swap(int a[],int i,int j)
 {
-    int i, j;
-    for (i = 0; i < n - 1; ++i)
+    int t=a[i];
+    a[i]=a[j];
+    a[j]=t;
+}
+void sort(int id[],int profit[],int deadline[],int n)
+{
+    for(int i=0;i<n-1;i++)
     {
-        for (j = 0; j < n - i - 1; ++j)
+        for(int j=0;j<n-i-1;j++)
         {
-            if (profit[j] < profit[j + 1])
+            if(profit[j]<profit[j+1])
             {
-                profit[j] = profit[j] ^ profit[j + 1];
-                profit[j + 1] = profit[j] ^ profit[j + 1];
-                profit[j] = profit[j] ^ profit[j + 1];
-                id[j] = id[j] ^ id[j + 1];
-                id[j + 1] = id[j] ^ id[j + 1];
-                id[j] = id[j] ^ id[j + 1];
-                deadline[j] = deadline[j] ^ deadline[j + 1];
-                deadline[j + 1] = deadline[j] ^ deadline[j + 1];
-                deadline[j] = deadline[j] ^ deadline[j + 1];
+                swap(profit,i,j);
+                swap(id,i,j);
+                swap(deadline,i,j);
             }
         }
     }
@@ -25,44 +38,46 @@ void sort(int id[], int profit[], int deadline[], int n)
 int main()
 {
     int n;
-    printf("Enter number of jobs\n");
+    printf("Enter number of items\n");
     scanf("%d",&n);
-    int id[n], profit[n], deadline[n];
-    int i;
-    int maxi = INT_MIN;
-    for (i = 0; i < n; ++i)
+    int profit[n],id[n],deadline[n];
+    int maxi=INT_MIN;
+    for(int i=0;i<n;i++)
     {
-        printf("Enter id, profit, deadline\n");
-        scanf("%d %d %d", &id[i], &profit[i], &deadline[i]);
-        // maxi = max(maxi, id[i]);
+        printf("Enter id,profit,deadline\n");
+        scanf("%d %d %d",&id[i],&profit[i],&deadline[i]);
+        // maxi=max(maxi,deadline[i]);
         if(deadline[i]>maxi)
-            maxi=deadline[i];
+        maxi=deadline[i];
     }
-    sort(id, profit, deadline, n);
-    int max_dead[maxi + 1];
-    for (i = 0; i <= maxi; i++)
-        max_dead[i] = -1;
-    int count = 0;
-    int totalProfit = 0;
-    for (i = 0; i < n; i++)
+    sort(id,profit,deadline,n);
+    int max_dead[maxi+1];
+    for(int i=0;i<=maxi;i++)
+        max_dead[i]=-1;
+    int count=0;
+    int profi=0;
+    int jobs_id[n];
+    for(int i=0;i<n;i++)
     {
-        int j = deadline[i];
-        while (j > 0)
+        int j=deadline[i];
+        while(j>0)
         {
-            if (max_dead[j] == -1)
+            if(max_dead[j]==-1)
             {
+                jobs_id[count]=id[i];
                 count++;
-                totalProfit += profit[i];
-                max_dead[j] = id[i]; // inserting job id at that postition at which the job is being done
-                break;
-            }
-            else
-            {
-                j--;
-            }
+                profi+=profit[i];
+                max_dead[j]=1;//assign anything apart from -1
+                break; 
+           }
+           else j--;
         }
     }
-    printf("Profit is %d\n", totalProfit);
-    printf("Number of jobs done is %d\n", count);
-
+    printf("Jobs done are ->%d,Profit is->%d\n",count,profi);
+    printf("Job ids order in which they were performed are given-> ");
+    for(int i=0;i<count;i++)
+    {
+        printf("%d ",jobs_id[i]);
+    }
+    printf("\n");
 }
